2e396a72637e730dddb6d18423c2332788130e8a
Mxy <1764732715@qq.com>
2018-11-27 20:49:37 +0800

    swiper

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,8 @@
 require.config({
 	paths:{
 		'jquery':'../libs/jquery.min',
-		'mui':'../libs/mui.min'
+		'mui':'../libs/mui.min',
+		'swiper':'../libs/swiper-4.1.6.min',
+		'better':'../libs/better-scroll'
 	}
 })
--- src/js/page/index.js
+++ src/js/page/index.js
@@ -1,5 +1,15 @@
 require(['./js/common.js'],function(){
-	require(['jquery','mui'],function($,mui){
-		console.log($)
+	require(['jquery','mui','swiper','better'],function($,mui,swiper,better){
+		init();
+		
+		function init(){
+			initSwiper();
+		}
+		
+		function initSwiper(){
+			var myswiper = new swiper('.swiper-container',{
+				
+			})
+		}
 	})
 })
